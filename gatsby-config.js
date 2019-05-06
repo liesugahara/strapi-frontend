67ac997e9b970f078104b3e0ddc8b8abcbe2016c
liesugahara <lie.sugahara@gmail.com>
2019-05-06 11:16:39 -0500

    Update config-gatsby.js and .gitingnore file

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: process.env.DEPLOY_URL
+          ? "https://strapi-demo-admin.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'article',
           'user'
